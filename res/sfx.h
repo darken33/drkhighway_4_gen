95030cff978a8aa8751cf35d43636eb5a4fed74a
Bousquet <philippe.bousquet@accenture.com>
2017-02-24 19:30:11 +0100

    Alpha version rev 2

--- res/sfx.h
+++ res/sfx.h
@@ -2,12 +2,11 @@
 #define _RES_SFX_H_
 
 extern const u8 brake_sfx[13312];
-extern const u8 button_sfx[256];
 extern const u8 cash_sfx[7168];
 extern const u8 crash_sfx[25088];
 extern const u8 horn_sfx[3584];
-extern const u8 horn2_sfx[256];
-extern const u8 motor_sfx[14592];
-extern const u8 takeoff_sfx[256];
+extern const u8 horn2_sfx[8704];
+extern const u8 motor_sfx[46080];
+extern const u8 plane_sfx[39424];
 
 #endif // _RES_SFX_H_
